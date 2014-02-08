49df4b563e7af78a2fcbc4c85699f74cc2b42e1b
rajuchavan <raju_chavan@yahoo.com>
2014-02-08 09:15:53 +0000

    first checkin

---
+++ Orchard.Web/Modules/Iota.eCommerce/AdminMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Routing;
+using Orchard.Environment;
+using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace Iota.eCommerce {
+    public class AdminMenu : INavigationProvider
+    {
+        public Localizer T { get; set; }
+
+        public string MenuName
+        {
+            get { return "admin"; }
+        }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.Add(T("Iota eCommerce"), "2", BuildMenu);
+        }
+
+        private void BuildMenu(NavigationItemBuilder menu)
+        {
+            menu.LinkToFirstChild(false);
+            menu.Add(T("Category"), "1.1", item =>
+                item.Action("List", "Admin", new { area = "Contents", id = "Category" }));
+
+            menu.Add(T("Product"), "1.2", item =>
+                item.Action("List", "Admin", new { area = "Contents", id = "Product" }));
+
+            //menu.Add(T("Movie Lookup"), "1.2", item =>
+            //    item.Action("Index", "MovieLookup", new { area = "Pluralsight.Movies" }).Permission(Permissions.LookupMovie));
+
+            //menu.Add(T("Actors"), "2.0", item =>
+            //    item.Action("Index", "ActorsAdmin", new { area = "Pluralsight.Movies" }));
+        }
+    }
+}
